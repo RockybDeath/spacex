6718b1ff94d6f5191f689a9683c1d84d0d7e5b56
RockybDeath <mdnvalery@yahoo.com>
2020-08-05 23:19:47 +0300

    animations in menu become sequential

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,8 @@ export const menuOpen = () => {
     const allBars = document.querySelectorAll(".bar");
     const menuSystem = document.querySelector("#menu");
     const menuNavigation = document.querySelector("#menu-navigation");
+    const navLinksPrimary = document.querySelectorAll(".primary");
+    const navLinksPrimaryArray = Array.from(navLinksPrimary);
     
     allBars.forEach( (item, i ) => {
         item.style.background = "black";
@@ -34,14 +36,31 @@ export const menuOpen = () => {
             
         }, queue: false
     });
-    // menuSystem.style.width = "350px";
-    // menuSystem.style.opacity = "1";
-    // menuNavigation.style.opacity = "1";
+    navLinksPrimary.forEach( item => {
+        $(item).attr("visibility", "inherit");
+    });
+    function animation(list) {
+        if (list.length === 0) {
+            return;
+        }
+        var $el = list.shift();
+        $($el).animate({
+            opacity: "1"
+        }, {
+            duration: 200,
+            easing: "swing",
+            complete: function(){
+                animation(list);   
+            }, queue: false
+        });
+    }
+    animation(navLinksPrimaryArray);
 };
 export const menuClose = () => {
     const allBars = document.querySelectorAll(".bar");
     const menuSystem = document.querySelector("#menu");
     const menuNavigation = document.querySelector("#menu-navigation");
+    const navLinksPrimary = document.querySelectorAll(".primary");
     
     allBars.forEach( (item, i ) => {
         item.style.background = "white";
@@ -72,7 +91,18 @@ export const menuClose = () => {
             menuNavigation.style.visibility = "hidden";
         }, queue: false
     });
-    menuNavigation.style.visibility = "hidden";
+    navLinksPrimary.forEach( item => {
+        $(item).attr("visibility", "hidden");
+        // console.log($(item).attr("style").attr("opacity", "0"));
+        $(item).css("opacity","0");
+        // $(item).animate({
+        //     opacity: "1"
+        // }, {
+        //     duration: 100,
+        //     easing: "swing",
+        //     queue: false
+        // });
+    })
     // menuSystem.style.width = "0";
     // menuSystem.style.opacity = "0";
     // menuNavigation.style.opacity = "0";
